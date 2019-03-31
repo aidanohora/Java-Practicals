e19074bc16a1c4f68331e24d0108ea1db11885d0
aidanohora <46319569+aidanohora@users.noreply.github.com>
2019-03-31 17:18:22 +0100

    Updated to use Stringbuilder class

--- Hangman.java
+++ Hangman.java
@@ -1,65 +1,41 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class Hangman {
+public class P4Q4 {
     public static void main(String[] args) {
-        System.out.println("Welcome to hangman!");
-        String[] words = {"hay", "elephant", "java", "magic"};
-        boolean repeat = true;
-        while (repeat == true) {
-            int index = (int) (Math.random() * words.length);
-            String word = words[index].toLowerCase();
-            Scanner input = new Scanner(System.in);
-            boolean wordGuessed = false;
-            String correctGuesses = "";
-            int incorrectGuesses = 0;
-            while (wordGuessed == false) {
-                char[] hiddenWord = new char[word.length() + 2];
-                hiddenWord[0] = '[';
-                for (char letter : word.toCharArray()) {
-                    if (correctGuesses.indexOf(letter) == -1) {
-                        hiddenWord[word.indexOf(letter) + 1] = '*';
-                    } else {
-                        boolean searching = true;
-                        int foundAt = word.indexOf(letter);
-                        hiddenWord[foundAt + 1] = letter;
-                        while (searching == true) {
-                            foundAt = word.indexOf(letter, foundAt+1);
-                            if (foundAt == -1) {
-                                searching = false;
-                            } else {
-                                hiddenWord[foundAt + 1] = letter;
-                            }
-                        }
+        String[] words = {"hello", "world", "elephant"};
+        int index = (int) (Math.random() * words.length);
+        String word = words[index].toLowerCase();
+        System.out.println(word);
+        StringBuilder display = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            display.append('*');
+        }
+        StringBuilder correctGuesses = new StringBuilder();
+        Scanner input = new Scanner(System.in);
+        boolean stillPlaying = true;
+        int misses = 0;
+        while (stillPlaying) {
+            System.out.println(display);
+            System.out.println("Guess a letter: ");
+            char guess = input.next().charAt(0);
+            if (correctGuesses.toString().indexOf(guess) != -1) {
+                System.out.println(guess + " is already in the word.");
+            } else if (word.indexOf(guess) != -1) {
+                System.out.println(guess + " is in the word!");
+                correctGuesses.append(guess);
+                for (int i = 0; i < display.length(); i++) {
+                    if (word.charAt(i) == guess) {
+                        display.setCharAt(i, guess);
                     }
                 }
-                hiddenWord[word.length() + 1] = ']';
-                String progressDisplay = "";
-                for (char element : hiddenWord) {
-                    progressDisplay += element;
-                }
-                if (progressDisplay.indexOf('*') == -1) {
-                    wordGuessed = true;
-                    break;
-                }
-                System.out.println("Guess a letter: " + progressDisplay);
-                char guess = input.next().charAt(0);
-                if (correctGuesses.indexOf(guess) != -1) {
-                    System.out.printf("%s has already been found in the word!%n%n", guess);
-                } else if (word.indexOf(guess) != -1) {
-                    correctGuesses += guess;
-                } else {
-                    System.out.printf("%s is not in the word.%n%n", guess);
-                    incorrectGuesses += 1;
-                }
+            } else {
+                System.out.println("Incorrect guess.");
+                misses++;
             }
-            System.out.printf("%nYou win! The word is '%s'. You missed %s times.%n%n", word, incorrectGuesses);
-            System.out.println("Enter 'y' to play again or any other character to exit: ");
-            String toContinue = input.next().toLowerCase();
-            System.out.print(toContinue);
-            if (toContinue.equals("y") == false) {
-                repeat = false;
+            if (display.toString().indexOf('*') == -1) {
+                stillPlaying = false;
             }
         }
+        System.out.println("You won with " + misses + " misses.");
     }
 }
