3dbae3b4718c4d1abc3a826596f0dbfd0d3b972b
aidanohora <46319569+aidanohora@users.noreply.github.com>
2019-03-31 17:19:28 +0100

    Updated to use Stringbuilder class

--- Palindrome.java
+++ Palindrome.java
@@ -1,25 +1,16 @@
 import java.util.Scanner;
 
 public class Palindrome {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a word to check if it's a palindrome: ");
-        String string = input.next();
-        if (isPalindrome(string)) {
-            System.out.printf("%s is a palindrome.", string);
-        } else {
-            System.out.printf("%s is not a palindrome.", string);
+        System.out.print("Enter a string: ");
+        String inputString = input.next();
+        StringBuilder sb = new StringBuilder(inputString);
+        for (int i = 0; i < sb.length(); i++) {
+            char preCypher = sb.charAt(i);
+            sb.setCharAt(i, ++preCypher);
         }
-    }
-
-    public static boolean isPalindrome(String string) {
-        string = string.toLowerCase();
-        for (int i = 0; i <= (string.length()/2); i++) {
-            if (string.charAt(i) != string.charAt((string.length()-1)-i)) {
-                return false;
-            }
-        }
-        return true;
+        System.out.println(sb);
     }
 
 }
